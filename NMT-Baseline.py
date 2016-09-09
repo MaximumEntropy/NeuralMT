c823d7d7ad70bcb2760c335c15948776dc292977
Sandeep Subramanian <sandeep.subramanian@gmail.com>
2016-09-09 16:21:35 -0400

    Added logging, fixed some implementation bugs

--- NMT-Baseline.py
+++ NMT-Baseline.py
@@ -6,6 +6,7 @@ import codecs
 from collections import Counter
 import math
 import argparse
+import logging
 
 sys.path.append('/u/subramas/Research/SanDeepLearn/')
 
@@ -43,11 +44,28 @@ parser.add_argument(
         help="path to dev tgt sentences",
         required=True
     )
-
 parser.add_argument(
         "-batch_size",
         "--batch_size",
         help="batch size",
+        required=True
+    )
+parser.add_argument(
+        "-pad",
+        "--pad_sentence",
+        help="pad sentences with <s> and </s> True/False",
+        action="store_true"
+    )
+parser.add_argument(
+        "-exp",
+        "--experiment_name",
+        help="name of the experiment",
+        required=True
+    )
+parser.add_argument(
+        "-eval_freq",
+        "--evaluation_frequency",
+        help="How frequently to evaluate model",
         required=True
     )
 
@@ -58,6 +76,26 @@ data_path_train_tgt = args.train_tgt_sentences
 data_path_dev_src = args.dev_src_sentences
 data_path_dev_tgt = args.dev_tgt_sentences
 batch_size = int(args.batch_size)
+experiment_name = args.experiment_name
+eval_freq = int(args.evaluation_frequency)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='log/%s' %(experiment_name),
+    filemode='w'
+)
+
+# define a new Handler to log to console as well
+console = logging.StreamHandler()
+# optional, set the logging level
+console.setLevel(logging.INFO)
+# set a format which is the same for console use
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
 def partition_data(src_lines, tgt_lines):
     train_src = src_lines[:int(0.8 * len(src_lines))]
@@ -73,8 +111,9 @@ def get_vocab(lines):
     for line in lines:
         for word in line:
             vocab.add(word)
-    vocab.add('<s>')
-    vocab.add('</s>')
+    if args.pad_sentence:
+        vocab.add('<s>')
+        vocab.add('</s>')
     vocab.add('<NULL>')
     word2ind = {}
     ind2word = {}
@@ -85,8 +124,9 @@ def get_vocab(lines):
     return vocab, word2ind, ind2word
 
 def prepare_batch(src_sentences, tgt_sentences):
-    src_sentences = [['<s>'] + sent + ['</s>'] for sent in src_sentences]
-    tgt_sentences = [['<s>'] + sent + ['</s>'] for sent in tgt_sentences]
+    if args.pad_sentence:
+        src_sentences = [['<s>'] + sent + ['</s>'] for sent in src_sentences]
+        tgt_sentences = [['<s>'] + sent + ['</s>'] for sent in tgt_sentences]
     src_lens = [len(sent) for sent in src_sentences]
     tgt_lens = [len(sent) for sent in tgt_sentences]
     max_src_len = max(src_lens)
@@ -94,9 +134,7 @@ def prepare_batch(src_sentences, tgt_sentences):
     src_sentences = [[src_word2ind[word] for word in sent] + [src_word2ind['<NULL>']] * (max_src_len - len(sent)) for sent in src_sentences]
     tgt_sentences_inp = [[tgt_word2ind[word] for word in sent[:-1]] + [tgt_word2ind['<NULL>']] * (max_tgt_len - len(sent)) for sent in tgt_sentences]
     tgt_sentences_op = [[tgt_word2ind[word] for word in sent[1:]] + [tgt_word2ind['<NULL>']] * (max_tgt_len - len(sent)) for sent in tgt_sentences]
-    tgt_mask = np.zeros((len(tgt_sentences), len(tgt_sentences_op[0]))).astype(np.float32)
-    for ind, tgt_len in enumerate(tgt_lens):
-        tgt_mask[ind][:tgt_len - 1] = 1.
+    tgt_mask = np.array([([1] * (l - 1)) + ([0] * (max_tgt_len - l)) for l in tgt_lens]).astype(np.float32)
     src_sentences = np.array(src_sentences).astype(np.int32)
     tgt_sentences_inp = np.array(tgt_sentences_inp).astype(np.int32)
     tgt_sentences_op = np.array(tgt_sentences_op).astype(np.int32)
@@ -104,7 +142,8 @@ def prepare_batch(src_sentences, tgt_sentences):
     return src_sentences, tgt_sentences_inp, tgt_sentences_op, src_lens, tgt_mask
 
 def prepare_evaluation_batch(src_sentences):
-    src_sentences = [['<s>'] + sent + ['</s>'] for sent in src_sentences]
+    if args.pad_sentence:
+        src_sentences = [['<s>'] + sent + ['</s>'] for sent in src_sentences]
     src_lens = [len(sent) for sent in src_sentences]
     max_src_len = max(src_lens)
     src_sentences = [[src_word2ind[word] for word in sent] + [src_word2ind['<NULL>']] * (max_src_len - len(sent)) for sent in src_sentences]
@@ -113,8 +152,9 @@ def prepare_evaluation_batch(src_sentences):
 
 def decode_batch(src_sentences):
     src_sentences, src_lens = prepare_evaluation_batch(src_sentences)
-    decode_state = np.array([[tgt_word2ind['<s>']] for sent in batch_tgt_inp]).astype(np.int32)
+    decode_state = np.array([[tgt_word2ind['<s>']] for _ in src_sentences]).astype(np.int32)
     is_decoding = [True] * len(src_sentences)
+    decode_lengths = [25] * len(src_sentences)
     decode_length = 1
     while any(is_decoding) and decode_length < 25:
         next_words = f_eval(
@@ -122,21 +162,29 @@ def decode_batch(src_sentences):
             decode_state,
             src_lens
         )
-        next_words = [x[0] for x in np.argmax(next_words, axis=2)]
+        next_words = [x[-1] for x in np.argmax(next_words, axis=2)]
         is_finished = [word == tgt_word2ind['</s>'] for word in next_words]
         for ind, item in enumerate(is_finished):
             if item == True:
                 is_decoding[ind] = False
+                decode_lengths[ind] = decode_length + 1
         decode_state = np.c_[decode_state, next_words].astype(np.int32)
         decode_length += 1
-    return decode_state
+    return decode_state, decode_lengths
 
 def decode_dev():
     decoded_sentences = []
-    for i in range(0, len(dev_src), batch_size):
-        print 'Decoding batch %d out of %d ' % (i, len(dev_src))
-        decoded_batch = decode_batch(dev_src[i:i + batch_size])
-        decoded_sentences += decoded_batch
+    for i in range(0, len(dev_src[:2000]), batch_size):
+        logging.info('Decoding batch %d out of %d ' % (i, len(dev_src)))
+        decoded_batch, decoded_lengths = decode_batch(dev_src[i:i + batch_size])
+        decoded_batch = [[tgt_ind2word[x] for x in sent[:length]] for sent, length in zip(decoded_batch, decoded_lengths)]
+        trimmed_decoded_batch = []
+        for sent in decoded_batch:
+            stop_ind = sent.index('</s>') if '</s>' in sent else len(sent)
+            stop_ind = stop_ind if args.pad_sentence else stop_ind + 1
+            start = 1 if args.pad_sentence else 0
+            trimmed_decoded_batch.append(sent[start:stop_ind])
+        decoded_sentences += trimmed_decoded_batch
     return decoded_sentences
 
 def bleu_stats(hypothesis, reference):
@@ -161,7 +209,7 @@ def get_validation_bleu(hypotheses, reference):
     stats = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
     for hyp, ref in zip(hypotheses, reference):
         stats += np.array(bleu_stats(hyp, ref))
-    return "%.3f" % (100*bleu(stats))
+    return 100 * bleu(stats)
 
 train_src = [line.strip().split() for line in codecs.open(data_path_train_src, 'r', encoding='utf-8')]
 train_tgt = [line.strip().split() for line in codecs.open(data_path_train_tgt, 'r', encoding='utf-8')]
@@ -169,18 +217,14 @@ train_tgt = [line.strip().split() for line in codecs.open(data_path_train_tgt, '
 dev_src = [line.strip().split() for line in codecs.open(data_path_dev_src, 'r', encoding='utf-8')]
 dev_tgt = [line.strip().split() for line in codecs.open(data_path_dev_tgt, 'r', encoding='utf-8')]
 
-src_vocab, src_word2ind, src_ind2word = get_vocab(train_src)
-tgt_vocab, tgt_word2ind, tgt_ind2word = get_vocab(train_tgt)
+src_vocab, src_word2ind, src_ind2word = get_vocab(train_src + dev_src)
+tgt_vocab, tgt_word2ind, tgt_ind2word = get_vocab(train_tgt + dev_tgt)
 
-print '=========================================================='
-print 'Finished reading data ...'
-print '=========================================================='
-print 'Number of training sentence-pairs : %d ' % (len(train_src))
-print 'Number of validation sentence-pairs : %d ' % (len(dev_src))
-print '=========================================================='
-print 'Source vocabulary size : %d ' % (len(src_vocab))
-print 'Target vocabulary size : %d ' % (len(src_vocab))
-print '=========================================================='
+logging.info('Finished reading data ...')
+logging.info('Number of training sentence-pairs : %d ' % (len(train_src)))
+logging.info('Number of validation sentence-pairs : %d ' % (len(dev_src)))
+logging.info('Source vocabulary size : %d ' % (len(src_vocab)))
+logging.info('Target vocabulary size : %d ' % (len(tgt_vocab)))
 
 src_inp = T.imatrix()
 tgt_inp = T.imatrix()
@@ -243,15 +287,13 @@ tgt_lstm_h_to_vocab = FullyConnectedLayer(
     activation='softmax',
 )
 
-params = src_embedding_layer.params + tgt_embedding_layer.params +     src_lstm_forward.params + src_lstm_backward.params +     tgt_lstm.params[:-1] + tgt_lstm_h_to_vocab.params # No need to learn h_0 for target LSTM
+params = src_embedding_layer.params + tgt_embedding_layer.params + src_lstm_forward.params + src_lstm_backward.params + tgt_lstm.params[:-1] + tgt_lstm_h_to_vocab.params # No need to learn h_0 for target LSTM
 
-print 'Model parameters ...'
-print '=========================================================='
-print 'Src Embedding dim : %d ' % (src_emb_dim)
-print 'Tgt Embedding dim : %d ' % (tgt_emb_dim)
-print 'Encoder BiLSTM dim : %d ' % (2 * src_lstm_forward.output_dim)
-print 'Decoder LSTM dim : %d ' %(tgt_lstm.output_dim)
-print '=========================================================='
+logging.info('Model parameters ...')
+logging.info('Src Embedding dim : %d ' % (src_emb_dim))
+logging.info('Tgt Embedding dim : %d ' % (tgt_emb_dim))
+logging.info('Encoder BiLSTM dim : %d ' % (2 * src_lstm_forward.output_dim))
+logging.info('Decoder LSTM dim : %d ' %(tgt_lstm.output_dim))
 
 #Get embedding matrices
 src_emb_inp = src_embedding_layer.fprop(src_inp)
@@ -289,24 +331,21 @@ cost = - (T.log(final_output[
 
 cost += beta * T.mean((tgt_lstm.h[:,:-1] ** 2 - tgt_lstm.h[:,1:] ** 2) ** 2) # Regularization of RNNs from http://arxiv.org/pdf/1511.08400v6.pdf
 
-print 'Computation Graph Node Shapes ...'
-print '=========================================================='
-print 'src embedding dim', src_emb_inp.eval({src_inp:src_inp_t}).shape
-print 'tgt embedding dim', tgt_emb_inp.eval({tgt_inp:tgt_inp_t}).shape
-print 'encoder forward dim', src_lstm_forward.h.dimshuffle(1, 0, 2).eval({src_inp:src_inp_t}).shape
-print 'encoder backward dim', src_lstm_backward.h.dimshuffle(1, 0, 2).eval({src_inp:src_inp_t}).shape
-print 'encoder hidden state shape', encoder_representation.eval({src_inp:src_inp_t}).shape
-print 'encoder final state shape', encoder_final_state.eval({src_inp:src_inp_t, src_lens:src_lens_t}).shape
-print 'decoder hidden state shape', tgt_lstm.h.dimshuffle(1, 0, 2).eval({src_inp: src_inp_t, tgt_inp:tgt_inp_t, src_lens:src_lens_t}).shape
-print 'attention vectors shape', attention.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape
-print 'decoder + attention shape', target_representation.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape
-print 'tgt vocab projection shape', proj_output_rep.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape
-print 'tgt vocab softmax shape', final_output.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape
-print 'cost', cost.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, tgt_op:tgt_op_t, src_lens:src_lens_t, tgt_mask:tgt_mask_t})
-print '=========================================================='
+logging.info('Computation Graph Node Shapes ...')
+logging.info('src embedding dim : %s ' % (src_emb_inp.eval({src_inp:src_inp_t}).shape,))
+logging.info('tgt embedding dim : %s' % (tgt_emb_inp.eval({tgt_inp:tgt_inp_t}).shape,))
+logging.info('encoder forward dim : %s' % (src_lstm_forward.h.dimshuffle(1, 0, 2).eval({src_inp:src_inp_t}).shape,))
+logging.info('encoder backward dim : %s' % (src_lstm_backward.h.dimshuffle(1, 0, 2).eval({src_inp:src_inp_t}).shape,))
+logging.info('encoder hidden state shape : %s' % (encoder_representation.eval({src_inp:src_inp_t}).shape,))
+logging.info('encoder final state shape : %s' % (encoder_final_state.eval({src_inp:src_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('decoder hidden state shape : %s' % (tgt_lstm.h.dimshuffle(1, 0, 2).eval({src_inp: src_inp_t, tgt_inp:tgt_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('attention vectors shape : %s' % (attention.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('decoder + attention shape : %s' % (target_representation.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('tgt vocab projection shape : %s' % (proj_output_rep.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('tgt vocab softmax shape : %s' % (final_output.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, src_lens:src_lens_t}).shape,))
+logging.info('cost : %.3f' % (cost.eval({tgt_inp:tgt_inp_t, src_inp:src_inp_t, tgt_op:tgt_op_t, src_lens:src_lens_t, tgt_mask:tgt_mask_t})))
 
-print 'Compiling theano functions ...'
-print '=========================================================='
+logging.info('Compiling theano functions ...')
 
 updates=Optimizer(clip=5.0).adam(
     cost=cost,
@@ -325,12 +364,8 @@ f_eval = theano.function(
 )
 
 num_epochs = 100
-print 'Training network ...'
-print '=========================================================='
+logging.info('Training network ...')
 for i in range(num_epochs):
-    dev_predictions = decode_dev()
-    print 'Epoch : %d dev BLEU : %.3f' % (i, get_validation_bleu(dev_predictions, dev_tgt))
-    print '=========================================================='
     for j in xrange(0, len(train_src), batch_size):
         batch_src, batch_tgt_inp, batch_tgt_op, batch_src_lens, batch_tgt_mask = prepare_batch(train_src[j:j + batch_size], train_tgt[j:j + batch_size])
         entropy = f_train(
@@ -340,4 +375,6 @@ for i in range(num_epochs):
             batch_src_lens,
             batch_tgt_mask
         )
-        print 'Epoch : %d Minibatch : %d Loss : %.3f' % (i, j, entropy)
+        logging.info('Epoch : %d Minibatch : %d Loss : %.3f' % (i, j, entropy))    
+    dev_predictions = decode_dev()
+    logging.info('Epoch : %d dev BLEU : %.3f' % (i, get_validation_bleu(dev_predictions, dev_tgt[:len(dev_predictions)])))
